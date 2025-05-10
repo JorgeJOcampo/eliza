e776d0bd0fbf46742bd50470d90bfa136c67b2ad
Jorge Ocampo <jorge.ocampo@mercadolibre.com>
2025-05-10 17:45:17 +0900

    step 1

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+import "slices"
+
+var QUIT_WORDS = []string{"exit", "quit", "bye", "goodbye", "goodnight", "good morning", "night", "morning"}
+
 func main() {
-	print("Hello, World!\n")
+	fmt.Println("Hello! This is Eliza!\n")
+	var input string
+	for !slices.Contains(QUIT_WORDS, input) {
+		fmt.Scan(&input)
+	}
 }
